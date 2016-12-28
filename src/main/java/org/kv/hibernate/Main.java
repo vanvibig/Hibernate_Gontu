ba4a77bde38b4a83ddfef02918752f566360c2c6
vanvibig <vanvibig@gmail.com>
2016-12-28 19:49:30 +0700

    tut 17

--- src/main/java/org/kv/hibernate/Main.java
+++ src/main/java/org/kv/hibernate/Main.java
@@ -14,6 +14,8 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
+		Student student = null;
+
 		SessionFactory sessionFactory = new Configuration()
 				.configure()
 				.buildSessionFactory();
@@ -21,15 +23,24 @@ public class Main {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-		Student student = (Student) session.get(Student.class, 1);
+		student = (Student) session.get(Student.class, 1);
 		System.out.println("Student object retrieved: name = "
 				+ student.getStudent_name());
 
-		student.setStudent_name("KV id = 1");
-
 		session.getTransaction().commit();
 		session.close();
+
+		student.setStudent_name("KV in detatch state");
+
+		Session session2 = sessionFactory.openSession();
+		session2.beginTransaction();
+
+		session2.update(student);
+		session2.getTransaction().commit();
+		session2.close();
+
 		sessionFactory.close();
+
 
 	}
 }
